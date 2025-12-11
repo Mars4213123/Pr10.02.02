47d4a4c9bb965d9f7372238b65e58977da1fcce1
officialarcode <officialarcodemusic06@gmail.com>
2025-12-11 17:04:10 +0500

    фыв

--- APIGigaChat_Kantuganov/Program.cs
+++ APIGigaChat_Kantuganov/Program.cs
@@ -8,7 +8,7 @@ namespace APIGigaChat_Kantuganov
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//
         {
         }
     }
